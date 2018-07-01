86d107d093563c1a418dfc2f0fa99bbd7e86240c
Uthaeus <romanlavery@gmail.com>
2018-07-01 11:09:44 -0600

    Added specs to navigation helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,14 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'capybara/poltergeist'
-require 'factory_girl_rails'
-require 'capybara/rspec'
 
-config.include Devise::Test::IntegreationHelpers, type: :feature 
-config.include FactoryGirl::Syntax::Methods
-Capybara.javascript_driver = :poltergeist
-Capybara.server = :puma
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -61,6 +54,15 @@ RSpec.configure do |config|
   config.after(:each) do 
     DatabaseCleaner.clean 
   end
+
+  require 'capybara/poltergeist'
+  require 'factory_girl_rails'
+  require 'capybara/rspec'
+
+  config.include Devise::Test::IntegrationHelpers, type: :feature 
+  config.include FactoryGirl::Syntax::Methods
+  Capybara.javascript_driver = :poltergeist
+  Capybara.server = :puma
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
