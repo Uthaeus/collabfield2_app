caa78509cf50444581183e537281a2f94066bd5c
Uthaeus <romanlavery@gmail.com>
2018-07-02 13:42:17 -0600

    Created UI to create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,21 @@
 class PostsController < ApplicationController
+  before_action :redirect_if_not_signed_in, only: [:new]
+
+  def new 
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      redirect_to root_path
+    end
+  end
 
   def show
     @post = Post.find(params[:id])
@@ -17,6 +34,10 @@ class PostsController < ApplicationController
   end
 
   private
+
+    def post_params
+      params.require(:post).permit(:content, :title, :category_id).merge(user_id: current_user.id)
+    end
 
     def posts_for_branch(branch)
       @categories = Category.where(branch: branch)
--- spec/features/posts/visit_single_post_spec.rb
+++ spec/features/posts/visit_single_post_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "Visit single post", :type => :feature do
   scenario "User goes to a single post from the home page", js: true do 
     post 
     visit root_path
-    page.find(".click-me").click
+    page.find(".single-post-card").click
     expect(page).to have_selector('body .modal')
     page.find('.interested a').click
     expect(page).to have_selector('#single-post-content p', text: post.content)
