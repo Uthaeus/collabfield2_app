ba90b6ef17a35d29a187ee8f3bef575197103878
Uthaeus <romanlavery@gmail.com>
2018-07-01 20:22:29 -0600

    Added popup window to show post content

---
+++ app/assets/javascripts/posts/modal.js
@@ -0,0 +1,13 @@
+$(document).on('turbolinks:load', function() {
+  $("body").on("click", ".single-post-card, .single-post-list", function() {
+    var posted_by = $(this).find('.post-content .posted-by').html();
+    var post_heading = $(this).find('.post-content h3').html();
+    var post_content = $(this).find('.post-content p').html();
+    var interested = $(this).find('.post-content .interested').attr('href');
+    $('.modal-header .posted-by').text(posted_by);
+    $('.loaded-data h3').text(post_heading);
+    $('.loaded-data p').text(post_content);
+    $('.loaded-data .interested a').attr('href', interested);
+    $('.myModal').modal('show');
+  });
+});
