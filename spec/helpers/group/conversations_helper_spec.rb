b5ddba33526ee140fef7c2bddd8536940ffc238a
Uthaeus <romanlavery@gmail.com>
2018-07-04 00:21:20 -0600

    Defined a load_group_messages_partial_path helper with specs

--- spec/helpers/group/conversations_helper_spec.rb
+++ spec/helpers/group/conversations_helper_spec.rb
@@ -36,4 +36,21 @@ RSpec.describe Group::ConversationsHelper, type: :helper do
       expect(helper.add_people_to_group_conv_list(conversation)).to include user 
     end
   end
+
+  context '#load_group_messages_partial_path' do 
+    let(:conversation) { create(:group_conversation) }
+
+    it "returns load_messages partial's path" do 
+      create_list(:group_message, 2, conversation_id: conversation.id)
+      expect(helper.load_group_messages_partial_path(conversation)).to eq(
+        'group/conversations/conversation/messages_list/link_to_previous_messages'
+      )
+    end
+
+    it "returns an empty partial's path" do 
+      expect(helper.load_group_messages_partial_path(conversation)).to eq(
+        'shared/empty_partial'
+      )
+    end
+  end
 end
