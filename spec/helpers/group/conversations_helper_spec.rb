560299cc26e37c82814a43ca0cf6b792cef0e6a3
Uthaeus <romanlavery@gmail.com>
2018-07-04 00:05:40 -0600

    Created add_people_to_group_conv-list with specs

--- spec/helpers/group/conversations_helper_spec.rb
+++ spec/helpers/group/conversations_helper_spec.rb
@@ -11,5 +11,29 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe Group::ConversationsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context '#add_people_to_group_conv_list' do 
+    let(:conversation) { create(:group_conversation) }
+    let(:current_user) { create(:user) }
+    let(:user) { create(:user) }
+    before(:each) do 
+      create(:contact,
+              user_id: current_user.id,
+              contact_id: user.id,
+              accepted: true)
+    end
+
+    it 'a user is not included in a list' do 
+      conversation.users << current_user
+      conversation.users << user 
+      helper.stub(:current_user).and_return(current_user)
+      expect(helper.add_people_to_group_conv_list(conversation)).not_to include user 
+    end
+
+    it 'a user is included in a list' do 
+      conversation.users << current_user
+      helper.stub(:current_user).and_return(current_user)
+      expect(helper.add_people_to_group_conv_list(conversation)).to include user 
+    end
+  end
 end
