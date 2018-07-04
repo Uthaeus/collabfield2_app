8954e7eee64dbfccfcf1e3e43c01cacf931b102f
Uthaeus <romanlavery@gmail.com>
2018-07-03 23:44:21 -0600

    Created a link on the nav bar to open a group conversation

--- app/helpers/shared/conversations_helper.rb
+++ app/helpers/shared/conversations_helper.rb
@@ -6,4 +6,14 @@ module Shared::ConversationsHelper
     not_created_by_user && unseen ? 'unseen-conv' : ''
   end
 
+  def group_conv_seen_status(conversation, current_user)
+    if current_user == nil
+      ''
+    else
+      not_created_by_user = conversation.messages.last.user_id != current_user.id
+      seen_by_user = conversation.messages.last.seen_by.include? current_user.id 
+      not_created_by_user && seen_by_user == false ? 'unseen-conv' : ''
+    end
+  end
+
 end
--- app/services/order_conversations_service.rb
+++ app/services/order_conversations_service.rb
@@ -6,7 +6,7 @@ class OrderConversationsService
 
   def call
     all_private_conversations = Private::Conversation.all_by_user(@user.id).includes(:messages)
-    all_group_conversations = @user.all_group_conversations.includes(:messages)
+    all_group_conversations = @user.group_conversations.includes(:messages)
     all_conversations = all_private_conversations + all_group_conversations
 
     return all_conversations = all_private_conversations.sort { |a, b| b.messages.last.created_at <=> a.messages.last.created_at }
