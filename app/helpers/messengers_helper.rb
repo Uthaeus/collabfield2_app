f93a561789231d6ce3cf8dd99fe4be459754db02
Uthaeus <romanlavery@gmail.com>
2018-07-05 15:09:13 -0600

    Created template for MessengerController index

--- app/helpers/messengers_helper.rb
+++ app/helpers/messengers_helper.rb
@@ -1,2 +1,10 @@
 module MessengersHelper
+
+  def conversations_list_item_partial_path(conversation)
+    if conversation.class == Private::Conversation
+      'messengers/index/conversations_list_item/private'
+    else
+      'messengers/index/conversations_list_item/group'
+    end
+  end
 end
