c41ff15dcad013f733b4b78145ce80c70130fe3d
Uthaeus <romanlavery@gmail.com>
2018-07-06 09:25:23 -0600

    Tracking test failures and bugs

--- spec/self_test/self_test_spec.rb
+++ spec/self_test/self_test_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Self_test' do 
-  context 'User locates post on homepage' do 
-    it 'finds a post link' do 
-      visit root_path
-      expect(page).to have_selector('.single-post-card')
-    end
-  end
+  let(:post) { create(:post) }
+  let(:user) { create(:user) }
+
+  
 end
