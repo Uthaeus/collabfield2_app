edc60eec095c03d693d370a2f8fa32526d3da841
Uthaeus <romanlavery@gmail.com>
2018-07-05 20:57:54 -0600

    Added javascript to messenger.js to show a different version for mobile devices

--- app/assets/javascripts/messenger.js
+++ app/assets/javascripts/messenger.js
@@ -7,3 +7,16 @@ $(document).on('turbolinks:load ajax:complete', function() {
     $('.conversation .messages-list .loading-more-messages').hide();
   }
 });
+
+$(".messenger .col-sm-2 ul").on("click", "a", function() {
+  var col_2_width = $('.messenger .col-sm-2').css('width');
+  var window_width = '' + $('.messenger').width() + 'px';
+
+  if (col_2_width == window_width) {
+    $('body nav').hide();
+    $('.messenger .col-sm-2').hide();
+    $('.messenger .col-sm-10').show();
+    $('body').css('margin', '0 0 0 0');
+    $('.messenger').css('height', '100vh');
+  }
+});
