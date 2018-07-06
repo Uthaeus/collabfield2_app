c24c020ecf0c583c2bbf43d09670c5729307a7a9
Uthaeus <romanlavery@gmail.com>
2018-07-05 21:51:47 -0600

    Created NotificationChannel

--- app/assets/javascripts/messenger.js
+++ app/assets/javascripts/messenger.js
@@ -6,17 +6,18 @@ $(document).on('turbolinks:load ajax:complete', function() {
     $('.load-more-messages', this)[0].click();
     $('.conversation .messages-list .loading-more-messages').hide();
   }
+
+  $(".messenger .col-sm-2 ul").on("click", "a", function() {
+    var col_2_width = $('.messenger .col-sm-2').css('width');
+    var window_width = '' + $('.messenger').width() + 'px';
+
+    if (col_2_width == window_width) {
+      $('body nav').hide();
+      $('.messenger .col-sm-2').hide();
+      $('.messenger .col-sm-10').show();
+      $('body').css('margin', '0 0 0 0');
+      $('.messenger').css('height', '100vh');
+    }
+  });
 });
 
-$(".messenger .col-sm-2 ul").on("click", "a", function() {
-  var col_2_width = $('.messenger .col-sm-2').css('width');
-  var window_width = '' + $('.messenger').width() + 'px';
-
-  if (col_2_width == window_width) {
-    $('body nav').hide();
-    $('.messenger .col-sm-2').hide();
-    $('.messenger .col-sm-10').show();
-    $('body').css('margin', '0 0 0 0');
-    $('.messenger').css('height', '100vh');
-  }
-});
